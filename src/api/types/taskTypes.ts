8728d1a60b523392085b81314d0131d70bb4e431
FTLPetrov <petyr.lubomirov.petrov@gmail.com>
2025-12-10 15:43:18 +0200

    Added - Task types and endpoints

---
+++ src/api/types/taskTypes.ts
@@ -0,0 +1,13 @@
+import type { TaskPriority } from "../../utils/types/TaskPriority";
+import type { TaskStatus } from "../../utils/types/TaskStatus";
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  assignedUserId: string;
+  createdAt: string;
+  updatedAt: string;
+}
